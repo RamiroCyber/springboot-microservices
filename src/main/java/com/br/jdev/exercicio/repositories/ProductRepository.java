f8c2a9ece3f892d869852e3e3450db062c25cc36
RamiroCyber <ramiro.dr@gmail.com>
2021-08-25 13:57:40 -0300

    criando consulta de todos os produtos de uma determinada categoria

--- src/main/java/com/br/jdev/exercicio/repositories/ProductRepository.java
+++ src/main/java/com/br/jdev/exercicio/repositories/ProductRepository.java
@@ -1,9 +1,20 @@
 package com.br.jdev.exercicio.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.br.jdev.exercicio.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	
+	@Query(value = "select p.name, p.price" 
+			+ "p.productIdentifier , p.description"
+			+ "from product p"
+			+ "join category c on p.category.id = c.id"
+			+ "where c.id = :categoryId" )
+	public List<Product> getProductByCategory(@Param("categoryId") long categoryId);
 }
